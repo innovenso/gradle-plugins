2c87cb403006173c0e111078d4ac656b715c70c2
Jurgen Lust <jurgen@innovenso.com>
2020-12-16 11:51:43 +0100

    added init task for Java

--- src/main/groovy/com/innovenso/gradle/plugin/java/JavaPluginExtension.groovy
+++ src/main/groovy/com/innovenso/gradle/plugin/java/JavaPluginExtension.groovy
@@ -3,4 +3,5 @@ package com.innovenso.gradle.plugin.java
 class JavaPluginExtension {
 	BigDecimal sourceCompatibility = 1.11
 	BigDecimal targetCompatibility = 1.11
+	String basePackage = 'com.innovenso'
 }
